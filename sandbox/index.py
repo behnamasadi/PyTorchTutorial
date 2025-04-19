dd2ee48d8f335411ba4e324b73e81d8f5f42c799
Behnam Asadi <behnam.asadi@gmail.com>
2025-04-19 22:42:10 +0200

    temp update

--- sandbox/index.py
+++ sandbox/index.py
@@ -1,16 +1,34 @@
 import torch
-import os
+import torch.nn as nn
+import torchvision.models as models
+import wandb
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-
-a = torch.tensor(data=[2., 3.], requires_grad=True)
-b = torch.tensor([6., 4.], requires_grad=True)
-q = 3*a**3-b**2
+model_resnet18_pretrained = models.resnet18(
+    weights=models.ResNet18_Weights.IMAGENET1K_V1)
 
 
-external_grad = torch.tensor([1., 1.])
-q.backward(gradient=external_grad)
-print(a.grad)
-print(9*a**2)
+model_resnet18_pretrained.fc = nn.Linear(in_features=512, out_features=10)
+# print(model_resnet18_pretrained.fc)
+
+
+resnet = models.resnet18(weights=None)
+print(resnet.conv1)
+
+resnet.conv1 = nn.Conv2d(
+    in_channels=1,  # <-- MNIST has 1 channel
+    out_channels=64,
+    kernel_size=7,
+    stride=2,
+    padding=3,
+    bias=False
+)
+
+resnet.fc = nn.Linear(512, 10)  # 10 classes for MNIST
+
+
+print(resnet.conv1)
+
+print("-----------------------------------")
+
+for param in resnet.parameters():
+    print("shape: ", param.shape)
