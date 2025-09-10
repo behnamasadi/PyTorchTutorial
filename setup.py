f1f746c44c46344cc6d6fd4586f0837d54fc6754
Behnam Asadi <behnam.asadi@gmail.com>
2025-09-10 20:41:54 +0200

    clean up

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pytorch-tutorial",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    python_requires=">=3.8",
-)
